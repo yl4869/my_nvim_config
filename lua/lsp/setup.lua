0b0e14432b275a92f20b82e964be15cd84afe01b
yunlang <yunlanglang@outlook.com>
2022-02-21 22:47:53 +0800

    my neovim config

---
+++ lua/lsp/setup.lua
@@ -0,0 +1,61 @@
+local lsp_installer = require "nvim-lsp-installer"
+
+-- config lsp_installer
+lsp_installer.settings({
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
+
+-- Include the servers you want to have installed by default below
+local servers = {
+    bashls = require "lsp.bash",
+    sumneko_lua = require "lsp.lua",
+    clangd = require "lsp.c_cpp",
+    cmake = require "lsp.cmake",
+    ltex = require "lsp.latex",
+    rust_analyzer = require "lsp.rust",
+    svls = require "lsp.verilog",
+}
+
+for name, _ in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found then
+    if not server:is_installed() then
+      print("Installing " .. name)
+      server:install()
+    end
+  end
+end
+
+lsp_installer.on_server_ready(function(server)
+  local opts = servers[server.name]
+  if opts then
+    opts.on_attach = function(_, bufnr)
+      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+      -- 绑定快捷键
+      require('keybindings').maplsp(buf_set_keymap)
+    end
+    opts.flags = {
+      debounce_text_changes = 150,
+    }
+     if server.name == "rust_analyzer" then
+        -- Initialize the LSP via rust-tools instead
+        require("rust-tools").setup {
+            -- The "server" property provided in rust-tools setup function are the
+            -- settings rust-tools will provide to lspconfig during init.            -- 
+            -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+            -- with the user's own settings (opts).
+            server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+        }
+        server:attach_buffers()
+    else
+        server:setup(opts)
+    end
+  end
+end)
---
+++ lua/plugins.lua
@@ -0,0 +1,27 @@
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'glepnir/zephyr-nvim'
+    use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'neovim/nvim-lspconfig' -- Collection of configurations for the built-in LSP client
+    use 'williamboman/nvim-lsp-installer'
+    -- nvim-cmp
+    use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
+    use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
+    use 'hrsh7th/cmp-path'     -- { name = 'path' }
+    use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
+    use 'hrsh7th/nvim-cmp'
+    -- vsnip
+    use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
+    use 'hrsh7th/vim-vsnip'
+    use 'rafamadriz/friendly-snippets'
+    -- lspkind
+    use 'onsails/lspkind-nvim'
+    use { 'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'},{'BurntSushi/ripgrep'} }
+        }
+    use 'simrat39/rust-tools.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'mfussenegger/nvim-dap'
+
+end)
